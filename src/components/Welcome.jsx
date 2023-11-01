1b174ad3e705fe59f7fd34106592dc7c3eac7426
João Vitor Bicalho <joaovitorbicalho.dasilva@gmail.com>
2023-10-31 22:19:24 -0300

    adicionando estilos globais

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -4,7 +4,7 @@ import "./Welcome.css"
 
 const Welcome = () => {
   return (
-    <div id='welcome'>
+    <div className='welcome'>
       <h2>Seja bem-vindo</h2>
       <p>Clique no botão abaixo para começar:</p>
       <button>Iniciar</button>
